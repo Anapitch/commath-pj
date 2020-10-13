b722fad6f01de17c858ba45906d52e0e8fce61d6
Anapitch <anapitcha.ya.61@ubu.ac.th>
2020-10-13 22:58:38 +0700

    Updates

---
+++ precache-manifest.92d1da735f812bfc2c616cc0db80626f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394d9780cbc86118bf4d316687a42064",
+    "url": "/commath-pj/index.html"
+  },
+  {
+    "revision": "0247b886bd09eb256403",
+    "url": "/commath-pj/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "40120c6e2a887bc207cc",
+    "url": "/commath-pj/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "0247b886bd09eb256403",
+    "url": "/commath-pj/static/js/2.5c44af66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commath-pj/static/js/2.5c44af66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40120c6e2a887bc207cc",
+    "url": "/commath-pj/static/js/main.3f8819fd.chunk.js"
+  },
+  {
+    "revision": "dd7e7165b43b49d2b8c0",
+    "url": "/commath-pj/static/js/runtime-main.5127bb74.js"
+  }
+]);
